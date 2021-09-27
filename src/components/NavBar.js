55011f8f3cc963e96095d27544010c9573c268b7
MandeepSingh-059 <1234mandeep1234@gmail.com>
2021-09-27 21:33:46 +0530

    testing2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -35,8 +35,8 @@ const NavBar = () => {
                     <NavbarToggler onClick={handleToggle} />
                     <Collapse isOpen={isOpenVal} navbar>
                         <Nav className="mr-auto" navbar>
-                            <NavItem><NavLink tag={RRNavLink} exact to="/">Home</NavLink></NavItem>
-                            <NavItem><NavLink tag={RRNavLink} to="/business">Business</NavLink></NavItem>
+                            <NavItem><NavLink tag={RRNavLink} exact to={process.env.PUBLIC_URL + '/'}>Home</NavLink></NavItem>
+                            <NavItem><NavLink tag={RRNavLink} to="/NewsFill/business">Business</NavLink></NavItem>
                             <NavItem><NavLink tag={RRNavLink} to="/entertainment">Entertainment</NavLink></NavItem>
                             <NavItem><NavLink tag={RRNavLink} to="/general">General</NavLink></NavItem>
                             <NavItem><NavLink tag={RRNavLink} to="/health">Health</NavLink></NavItem>
